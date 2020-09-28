f896fe04e186ff7441858312c309819679043ce2
sheraf ansari <sheraf.ansari@tatahealth.com>
2020-09-28 19:01:44 +0530

    added all files in repo

---
+++ src/test/java/com/motors/followup/FollowupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motors.followup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
